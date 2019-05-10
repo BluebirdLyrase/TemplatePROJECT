0c8c609b9795daf3ed11db09f6de71a3fe4df594
tewtew023 <38246059+tewtew023@users.noreply.github.com>
2019-05-10 18:22:45 +0700

    lil fix

--- src/truckEco.java
+++ src/truckEco.java
@@ -2,13 +2,13 @@ public class truckEco extends truckDecorator{
 
     @Override
     public double calTransportSpeed() {
-        return "0.0";
+        return 0.0;
     }
 
     @Override
     public double calTransportCost() {
         System.out.println("ADD ECO MODE");
-        return "0.0";
+        return 0.0;
     }
 
     public truckEco(Truck truck) {
--- src/truckFast.java
+++ src/truckFast.java
@@ -3,13 +3,13 @@ public class truckFast extends truckDecorator {
     @Override
     public double calTransportSpeed() {
         System.out.println("ADD FAST TRAVEL MODE");
-        return "0.0";
+        return 0.0;
     }
 
     @Override
     public double calTransportCost() {
         System.out.println("");
-        return "0.0";
+        return 0.0;
     }
 
     public truckFast(Truck truck) {
